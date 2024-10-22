3cb60a584996881e859aed4a182eb93a13869c8f
bifinbabu <bifinbabu088@gmail.com>
2024-10-22 21:28:49 +0530

    Lazy loading of youtube video implemented

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,11 @@ import axios from "axios";
 function App() {
   const [searchKey, setSearchKey] = useState("");
   const [meals, setMeals] = useState();
+  const [loading, setLoading] = useState(false); // New loading state
 
   const fetchData = async (searchKey) => {
     try {
+      setLoading(true); // Set loading to true when fetching data
       let response = await axios.get(
         `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchKey}`
       );
@@ -17,6 +19,8 @@ function App() {
       setMeals(response.data.meals);
     } catch (error) {
       console.log("Error", error);
+    } finally {
+      setLoading(false); // Set loading to false after fetching data
     }
   };
 
@@ -33,6 +37,7 @@ function App() {
         <button className="search-button" onClick={() => fetchData(searchKey)}>
           Search
         </button>
+        {loading && <p>Loading...</p>}
         {meals ? (
           meals.length > 0 ? (
             meals.map((meal) => (
@@ -43,16 +48,18 @@ function App() {
                   className="meal-image"
                 />
                 <p className="meal-name">{meal.strMeal}</p>
-                <iframe
-                  width="100%"
-                  height="315"
-                  src={meal.strYoutube.replace("watch?v=", "embed/")}
-                  title={meal.strMeal}
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                  className="meal-video"
-                ></iframe>
+                <div className="meal-video">
+                  <iframe
+                    width="100%"
+                    height="315"
+                    src={meal.strYoutube.replace("watch?v=", "embed/")}
+                    title={meal.strMeal}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    loading="lazy"
+                  ></iframe>
+                </div>
               </div>
             ))
           ) : (
